667ee3dbb8b9d0a04127adec67401328190d86b4
Omarkhaledd <79328900+Omarkhaledd@users.noreply.github.com>
2021-05-02 19:50:21 +0200

    Delete Microcontroller 1 directory

--- Microcontroller 1/Debug/UART.d
+++
@@ -1,9 +0,0 @@
-UART.o: ../UART.c ../UART.h ../UART_CONFIG.h ../CPU_CONFIGURATION.h \
- ../BIT_MATH.h ../STD_TYPES.h ../DIO.h ../DIO_CONFIG.h
-../UART.h:
-../UART_CONFIG.h:
-../CPU_CONFIGURATION.h:
-../BIT_MATH.h:
-../STD_TYPES.h:
-../DIO.h:
-../DIO_CONFIG.h:
