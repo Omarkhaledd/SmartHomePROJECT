3a2ee8ebaebed008f2969de093c63c21fb3dd166
Omarkhaledd <79328900+Omarkhaledd@users.noreply.github.com>
2021-05-02 20:05:01 +0200

    Delete Microcontroller 2/Debug directory

--- Microcontroller 2/Debug/SPI.d
+++
@@ -1,9 +0,0 @@
-SPI.o: ../SPI.c ../SPI.h ../SPI_CONFIG.h ../CPU_CONFIGURATION.h \
- ../BIT_MATH.h ../STD_TYPES.h ../DIO.h ../DIO_CONFIG.h
-../SPI.h:
-../SPI_CONFIG.h:
-../CPU_CONFIGURATION.h:
-../BIT_MATH.h:
-../STD_TYPES.h:
-../DIO.h:
-../DIO_CONFIG.h:
