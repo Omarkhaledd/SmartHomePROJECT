9b84c8172750f9a1fe3f650ba40f703e3e5a6a05
Omarkhaledd <79328900+Omarkhaledd@users.noreply.github.com>
2021-05-02 20:05:13 +0200

    Delete Microcontroller 1 directory

--- Microcontroller 1/Debug/DIO.d
+++
@@ -1,7 +0,0 @@
-DIO.o: ../DIO.c ../DIO.h ../DIO_CONFIG.h ../CPU_CONFIGURATION.h \
- ../BIT_MATH.h ../STD_TYPES.h
-../DIO.h:
-../DIO_CONFIG.h:
-../CPU_CONFIGURATION.h:
-../BIT_MATH.h:
-../STD_TYPES.h:
